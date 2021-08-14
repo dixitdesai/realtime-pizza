33e796c21c64f8d762389f6bfbeb5eee29f4eef8
Dixit Desai <dixitdesai2002@gmail.com>
2021-08-14 23:06:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen( 3000, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
